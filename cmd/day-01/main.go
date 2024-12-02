9f892f9e53769b1389384befe59ed0b089a3c2eb
Rob Cannon <rpcann@gmail.com>
2024-12-01 22:34:10 -0500

    Fix formatting for go

--- cmd/day-01/main.go
+++ cmd/day-01/main.go
@@ -9,62 +9,60 @@ import (
 	"strings"
 )
 
-
-
 func main() {
-    fmt.Println("Hello, World!")
-    var input = my.ReadFile("./inputs/day-01/input.txt")
+	fmt.Println("Hello, World!")
+	var input = my.ReadFile("./inputs/day-01/input.txt")
 
-    split1 := strings.Split(input, "\n")
+	split1 := strings.Split(input, "\n")
 
-    var left []int
-    var right []int
+	var left []int
+	var right []int
 
-    for _,element := range split1 {
-      s := strings.Split(element, "   ")
-      l, err := strconv.Atoi(s[0])
-      if err != nil {
-        log.Fatal(err)
-      }
-      r, err := strconv.Atoi(s[1])
-      if err != nil {
-        log.Fatal(err)
-      }
+	for _, element := range split1 {
+		s := strings.Split(element, "   ")
+		l, err := strconv.Atoi(s[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		r, err := strconv.Atoi(s[1])
+		if err != nil {
+			log.Fatal(err)
+		}
 
-      left = append(left, l)
-      right = append(right, r)
-    }
+		left = append(left, l)
+		right = append(right, r)
+	}
 
-    slices.Sort(left)
-    slices.Sort(right)
+	slices.Sort(left)
+	slices.Sort(right)
 
-    var distance []int
+	var distance []int
 
-    for i := range left {
-      distance = append(distance, my.Abs(left[i] - right[i]))
-    }
+	for i := range left {
+		distance = append(distance, my.Abs(left[i]-right[i]))
+	}
 
-    var n int = my.Sum(distance)
- 
-    fmt.Println("Day 01: Part 1")
-    fmt.Println(n)
+	var n int = my.Sum(distance)
 
-    var similarity []int
+	fmt.Println("Day 01: Part 1")
+	fmt.Println(n)
 
-    for i := range left {
-      x := left[i]
-      c := 0
-      for _,e := range right {
-        if e == x {
-          c++
-        }
-      }
-      similarity = append(similarity, x * c)
-    }
+	var similarity []int
 
-    var n2 int = my.Sum(similarity)
- 
-    fmt.Println("Day 01: Part 2")
-    fmt.Println(n2)
+	for i := range left {
+		x := left[i]
+		c := 0
+		for _, e := range right {
+			if e == x {
+				c++
+			}
+		}
+		similarity = append(similarity, x*c)
+	}
+
+	var n2 int = my.Sum(similarity)
+
+	fmt.Println("Day 01: Part 2")
+	fmt.Println(n2)
 
 }
--- cmd/day-02/main.go
+++ cmd/day-02/main.go
@@ -8,12 +8,12 @@ import (
 const DAY = 2
 
 func main() {
-    var input = my.ReadFile(fmt.Sprintf("./inputs/day-%02v/input.txt", DAY))
- 
-    fmt.Printf("Day %02v: Part 1\n", DAY)
-    fmt.Println(input)
- 
-    // fmt.Printf("Day %02v: Part 2\n", DAY)
-    // fmt.Println(input)
+	var input = my.ReadFile(fmt.Sprintf("./inputs/day-%02v/input.txt", DAY))
+
+	fmt.Printf("Day %02v: Part 1\n", DAY)
+	fmt.Println(input)
+
+	// fmt.Printf("Day %02v: Part 2\n", DAY)
+	// fmt.Println(input)
 
 }

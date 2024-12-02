335407d26ca5e5f1f4cdeb16640953aa10f2783c
Rob Cannon <rpcann@gmail.com>
2024-12-01 22:32:17 -0500

    Setup day 2

---
+++ cmd/day-02/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	my "robpc/advent-of-code-2024/internal/my"
+)
+
+const DAY = 2
+
+func main() {
+    var input = my.ReadFile(fmt.Sprintf("./inputs/day-%02v/input.txt", DAY))
+ 
+    fmt.Printf("Day %02v: Part 1\n", DAY)
+    fmt.Println(input)
+ 
+    // fmt.Printf("Day %02v: Part 2\n", DAY)
+    // fmt.Println(input)
+
+}
